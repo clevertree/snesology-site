baabd1fa12a8a9bd1ecb658b91b42bb4d92adab8
Ari Asulin <ari.asulin@gmail.com>
2023-09-28 21:30:33 -0400

    redirect demo to audiosource.io

--- pages/DemoPage.md
+++
@@ -1,4 +0,0 @@
-
-# SNESology Composer Demo
-Menu->View->Enable Fullscreen to render in landscape mode
-    [or load the Composer by itself](/composer)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,51 +1,52 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import themeStyles from '@/styles/default-theme.module.scss'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import Link from "next/link";
 import Image from "next/image";
 import {useRouter} from "next/router";
 import {useSearchParams} from "next/navigation";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const searchParams = useSearchParams()
-  const router = useRouter()
-  let content = <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+    const searchParams = useSearchParams()
+    const router = useRouter()
+    let content = <Component {...pageProps} />;
 
-  if (!pageProps.skipArticleTag)
-    content = <article className={themeStyles.article}>
-      {content}
-    </article>
+    if (!pageProps.skipArticleTag)
+        content = <article className={themeStyles.article}>
+            {content}
+        </article>
 
-  if (!!searchParams.get('contentOnly'))
-    return <div className={themeStyles.container}>
-      {content}
-    </div>
+    if (!!searchParams.get('contentOnly'))
+        return <div className={themeStyles.container}>
+            {content}
+        </div>
 
-  return (
-      <div className={themeStyles.container}>
-        <header className={themeStyles.header}>
-          <Link href="/">
-            <Image priority src="/logo-header.png" alt="Header Logo" className={themeStyles.logo} width={669}
-                   height={300}/>
-          </Link>
-        </header>
+    return (
+        <div className={themeStyles.container}>
+            <header className={themeStyles.header}>
+                <Link href="/">
+                    <Image priority src="/logo-header.png" alt="Header Logo" className={themeStyles.logo} width={669}
+                           height={300}/>
+                </Link>
+            </header>
 
-          <nav className={themeStyles.linkContainer}>
-              <Link href="/">home</Link>
-              <Link href="/about">about</Link>
-              <Link href="/contact">contact</Link>
-              <Link href="https://chat.paradigmthreat.net">chat</Link>
-          </nav>
+            <nav className={themeStyles.linkContainer}>
+                <Link href="/">home</Link>
+                <Link href="https://audiosource.io/demo">demo</Link>
+                <Link href="/about">about</Link>
+                <Link href="/contact">contact</Link>
+                <Link href="https://chat.paradigmthreat.net">chat</Link>
+            </nav>
 
-        {content}
+            {content}
 
-        <footer className={themeStyles.footer}>
-          <div>created by <a href="https://clevertree.net/">Ari Asulin</a></div>
-          {/*<hitCounter></hitCounter>*/}
-          <div>
-            [<a href="https://git.paradigmthreat.net/">git repository</a>]
-          </div>
-        </footer>
-      </div>
-  )
+            <footer className={themeStyles.footer}>
+                <div>created by <a href="https://clevertree.net/">Ari Asulin</a></div>
+                {/*<hitCounter></hitCounter>*/}
+                <div>
+                    [<a href="https://git.paradigmthreat.net/">git repository</a>]
+                </div>
+            </footer>
+        </div>
+    )
 }
